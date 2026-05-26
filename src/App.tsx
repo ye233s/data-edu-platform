a891635023a043565ac0c713760887ebe6cfbdaf
ye233s <15811668681@163.com>
2026-05-26 14:56:40 +0000

    修复构建配置问题

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "@/pages/Home";
-import Courses from "@/pages/Courses";
-import CourseDetail from "@/pages/CourseDetail";
-import Learn from "@/pages/Learn";
-import Achievements from "@/pages/Achievements";
-import Profile from "@/pages/Profile";
-import Login from "@/pages/Login";
-import Register from "@/pages/Register";
+import Home from "./pages/Home";
+import Courses from "./pages/Courses";
+import CourseDetail from "./pages/CourseDetail";
+import Learn from "./pages/Learn";
+import Achievements from "./pages/Achievements";
+import Profile from "./pages/Profile";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 export default function App() {
   return (
